e27161f050a4447aa793b0dd0bd01d7eed1afd00
andimardhotilla <andi.mardhotilla99@gmail.com>
2021-11-25 09:08:10 -0800

    initial commit

--- src/test/java/selenium/test/BodyEmailExtract.java
+++ src/test/java/selenium/test/BodyEmailExtract.java
@@ -1,13 +1,16 @@
-package qaautomation2.tugas34;
+package selenium.test;
 
+import org.openqa.selenium.By; 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
-public class BaseWebTestNo3{
+public class BodyEmailExtract{
 	ThreadLocal<WebDriver> driver = new ThreadLocal<WebDriver>();
 
 	@BeforeMethod
@@ -18,10 +21,20 @@ public class BaseWebTestNo3{
 		driver.get().manage().window().maximize();		
 	}
 	
+	@Test
+	public void testdalamiframe() {
+		String email = "automationtest";
+		driver.get().findElement(By.xpath("//input[@id='login']")).sendKeys(email);
+		driver.get().findElement(By.xpath("//i[@class='material-icons-outlined f36']")).click();
+		driver.get().switchTo().frame("ifmail");
+		WebElement body = driver.get().findElement(By.xpath("//body"));
+		String isi = body.getText();
+	    System.out.println("Body email: " + isi);
+	}
+	
 	@AfterMethod
 	public void quitChromeDriver() {
 		driver.get().quit();
 	}
 	
-
 }
--- src/test/java/selenium/test/FlightsBooking.java
+++ src/test/java/selenium/test/FlightsBooking.java
@@ -1,6 +1,6 @@
-package qaautomation2.tugas34;
+package selenium.test;
 
-import org.openqa.selenium.By;    
+import org.openqa.selenium.By;     
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -8,12 +8,12 @@ import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
-public class SeleniumTesNo4{
+public class FlightsBooking{
 	
 	@Test
-	public void tessequence() {
-	String asal = "Jakarta";
-	String tujuan = "Tokyo";
+	public void flightorder() {
+	String departure = "London";
+	String arrival = "Tokyo";
 	WebDriver driver;
 	WebDriverManager.chromedriver().setup();
 	driver = new ChromeDriver();
@@ -21,19 +21,19 @@ public class SeleniumTesNo4{
 	driver.manage().window().maximize();
 	driver.findElement(By.xpath("/html/body")).click();
 	driver.findElement(By.xpath("//span[normalize-space()='From']")).click();
-	driver.findElement(By.xpath("//input[@placeholder='From']")).sendKeys(asal);
+	driver.findElement(By.xpath("//input[@placeholder='From']")).sendKeys(departure);
 	utils.Utility.hardWait(5);
 	driver.findElement(By.xpath("//li[@id='react-autowhatever-1-section-0-item-0']")).click();
 	driver.findElement(By.xpath("//span[normalize-space()='To']")).click();
-	driver.findElement(By.xpath("//input[@placeholder='To']")).sendKeys(tujuan);
+	driver.findElement(By.xpath("//input[@placeholder='To']")).sendKeys(arrival);
 	utils.Utility.hardWait(5);
 	driver.findElement(By.xpath("//li[@id='react-autowhatever-1-section-0-item-1']")).click();
 	driver.findElement(By.xpath("//a[normalize-space()='Search']")).click();
 	
-	By harga = By.xpath("//body/div[@id='root']/div/div/div[@class='flightBody']/div[@class='flightsContainer makeFlex spaceBetween']/div[contains(@class,'listingLhs')]/div[@class='filterWrapper']/div[2]");
-	WebElement isi = driver.findElement(harga);
+	By price = By.xpath("//body/div[@id='root']/div/div/div[@class='flightBody']/div[@class='flightsContainer makeFlex spaceBetween']/div[contains(@class,'listingLhs')]/div[@class='filterWrapper']/div[2]");
+	WebElement tag = driver.findElement(price);
     System.out.println("Based on the booking's type, the top or the cheapest and the most expensive filght's price are" 
-	+ isi.getText());
+	+ tag.getText());
     
     driver.quit();
 	}
